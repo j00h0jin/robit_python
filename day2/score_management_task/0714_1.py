fcb1f91a382d7b99e359fee50eae9ee42d77945c
j00h0jin <hojin7710@daum.net>
2026-07-14 14:36:01 +0900

    ✅ 0714 과제1

--- day2/score_management_task/0714_1.py
+++ day2/score_management_task/0714_1.py
@@ -24,21 +24,120 @@ name,score
 '''
 
 import csv
+import json
+import numpy as np
 from pathlib import Path
 
-path = Path('./students.csv')
-
-if path.exists():
-    with open("students.csv", newline="", encoding="utf-8") as file:
-        reader = csv.reader(file)
-        header = next(reader)
-
-        for row in header:
-            name = row[0]
-            score = int(row[1]) # str -> int
-            print(name, score)
-
-else:
-    print("E")
+students_path = Path('.\day2\score_management_task\students.csv')
+clean_students_path = Path('.\day2\score_management_task\clean_students.csv')
+summary_path = Path('.\day2\score_management_task\summary.json')
 
 
+def readerWriter():
+    rows = []
+    scores = []
+    dict = {}
+    
+    if students_path.exists():
+        with open(students_path, newline="", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            header = next(reader)
+
+            for row in reader:
+                name = row[0]
+                if row[1].isdecimal():
+                    score = int(row[1])
+                    if score >= 0 and score <=100:
+                        valid_score = score
+                        print(name, valid_score)
+                        rows.append([name, valid_score])
+                    else:
+                        error = "허용 범위 초과"
+                        print(error)
+                else:
+                    error = "숫자 변환 실패"
+                    print(error)
+    else:
+        print("ERROR")
+    
+    if clean_students_path.exists():
+        with open(clean_students_path, "w", newline="", encoding="utf-8")as file:
+            writer = csv.writer(file)
+
+            writer.writerow(["name", "score"])
+            writer.writerows(rows)
+    
+    else:
+        print("ERROR")
+    
+    for row in rows:
+        scores.append(int(row[1]))
+    
+    mean = np.mean(scores)
+    max = int(np.max(scores))
+
+    for i in rows:
+        dict = {"count": len(rows), "mean": mean, "max": max}
+        
+    if summary_path.exists():
+        with open(summary_path, "w", encoding="utf-8") as file:
+            json.dump(dict, file, indent = 2)
+    
+    else:
+        print("ERROR")
+
+
+def dictReaderWriter():
+    rows = []
+    scores = []
+    dict = {}
+    
+    if students_path.exists():
+        with open(students_path, newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file) #
+
+            for row in reader:
+                name = row["name"]
+                if row["score"].isdecimal():
+                    score = int(row["score"]) #
+                    if score >= 0 and score <=100:
+                        valid_score = score
+                        print(name, valid_score)
+                        rows.append({"name": name, "score": valid_score})
+                    else:
+                        error = "허용 범위 초과"
+                        print(error)
+                else:
+                    error = "숫자 변환 실패"
+                    print(error)
+    else:
+        print("ERROR")
+
+    if clean_students_path.exists():
+        with open(clean_students_path, "w", newline="", encoding="utf-8")as file:
+            writer = csv.DictWriter(file, fieldnames=["name", "score"])
+
+            writer.writeheader
+            writer.writerows(rows)
+    
+    else:
+        print("ERROR")
+    
+    for row in rows:
+        scores.append(int(row["score"]))
+    
+    mean = np.mean(scores)
+    max = int(np.max(scores))
+    
+    for i in rows:
+        dict = {"count": len(rows), "mean": mean, "max": max}
+        
+    if summary_path.exists():
+        with open(summary_path, "w", encoding="utf-8") as file:
+            json.dump(dict, file, indent = 2)
+    
+    else:
+        print("ERROR")
+
+readerWriter()
+#dictReaderWriter()
